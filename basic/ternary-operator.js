21f6afe19af272c7b1e34c4c348afa8b0459680c
Vrushali Balaso Patil <91833514+vrushali2902@users.noreply.github.com>
2024-06-27 14:45:01 +0530

    Delete basic directory

--- basic/ternary-operator.js
+++
@@ -1,5 +0,0 @@
-let n = 45
-
-let drink = n > 5 ? "coffee" : "milk"
-
-console.log(drink)
--- basic/typeof.js
+++
@@ -1,9 +0,0 @@
-let numb=23
-numb = numb+""
-console.log(typeof numb)  //------------String
-
-let str="34"
-str = +str
-console.log(typeof str)  //------------number
-
-
